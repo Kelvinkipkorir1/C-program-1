a152f554ff1105977a31f8de95fcc06130d8c1fd
kelvin kipkorir <124672350+tylerrenz@users.noreply.github.com>
2023-04-10 15:13:48 +0300

    Add files via upload

---
+++ area.c
@@ -0,0 +1,12 @@
+//area of Circle
+#include<stdio.h>
+int main()
+{
+    int r;
+    float area;
+    printf("enter the radius");
+    scanf("%d",&r);
+    area=3.14*r*r;
+    printf("the area of the circle is %f", area);
+    return 0;
+}
